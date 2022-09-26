88d5fa3208cde53575b74e36b1df5b66240cc7b3
jpsilveirabastos <74983593+jpsilveirabastos@users.noreply.github.com>
2022-09-26 16:02:48 -0300

    Update db.py

--- Audiences/app/core/db.py
+++ Audiences/app/core/db.py
@@ -1,10 +1,10 @@
-# from .config import DBNAME, USER, PASSWORD, HOST
+from .config import DBNAME, USER, PASSWORD, HOST
 import psycopg2
 
 class Db:
 
     def connect():
-        conn = psycopg2.connect(dbname='postgres', user='postgres', password='8283', host='localhost')
+        conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST)
         cur = conn.cursor()
         return cur,conn
 
